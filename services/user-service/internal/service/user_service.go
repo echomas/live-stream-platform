e4f951db1ddc664be3a17227df291d3d55e4b7be
Jerrycans <echoapinkchen@gmail.com>
2025-11-07 16:41:40 +0800

    add proto gen files

--- services/user-service/internal/service/user_service.go
+++ services/user-service/internal/service/user_service.go
@@ -2,4 +2,9 @@ package service
 
 import (
 	"context"
+
+	commonPb "github.com/echomas/live-stream-platform/proto/common"
+	userPb "github.com/echomas/live-stream-platform/proto/user"
+	"live-stream-platform/pkg/jwt"
+	"live-stream-platform/pkg/utils"
 )
