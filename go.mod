a292d6df4632f60db657cb7417b731f491359ba2
Jerrycans <echoapinkchen@gmail.com>
2025-11-07 16:20:02 +0800

    add pkg/utils

--- go.mod
+++ go.mod
@@ -10,5 +10,6 @@ require (
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
 )
---
+++ pkg/utils/utils.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"encoding/hex"
+	"golang.org/x/crypto/bcrypt"
+	"math/rand"
+)
+
+// HashPassword 加密密码
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
+
+// CheckPassword 验证密码
+func CheckPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+// GenerateRandomString 生成随机字符串
+func GenerateRandomString(length int) (string, error) {
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", nil
+	}
+	return hex.EncodeToString(bytes)[:length], nil
+}
